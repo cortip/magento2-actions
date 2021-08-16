c68615ba2eb52fe4e866a277cb2f7d3a1bbffe80
Vygandas Pliasas <vygandas.pliasas@toptal.com>
2021-08-16 16:28:38 +0300

    do building of stuff before it's on current

--- config/pipelines/scripts/production/release_setup.sh
+++ config/pipelines/scripts/production/release_setup.sh
@@ -3,3 +3,17 @@
 set -e
 
 # operation to make before the switch of the release folder
+php bin/magento config:set dev/js/enable_js_bundling 1
+php bin/magento config:set dev/js/minify_files 1
+php bin/magento config:set dev/static/sign 0
+php bin/magento config:set dev/js/merge_files 0
+
+php bin/magento config:set dev/css/merge_css_files 1
+php bin/magento config:set dev/css/minify_files 1
+
+bin/magento deploy:mode:set production
+bin/magento setup:upgrade
+bin/magento setup:di:compile
+bin/magento setup:static-content:deploy -f
+bin/magento cache:clean
+bin/magento cache:flush
